8614394bb5b047d91e3f7f544d012423b42eed6b
Adam Matan <adam@matan.name>
2013-11-17 09:51:13 +0200

    Basic movie data

--- www/js/screenings.js
+++ www/js/screenings.js
@@ -1,9 +1,16 @@
-// parse a date in yyyy-mm-dd format
-function parseDate(input) {
-  var parts = input.split('-');
-  // new Date(year, month [, date [, hours[, minutes[, seconds[, ms]]]]])
-  var year  = parts[0];
-  var month = parts[1]-1; // months are 0-based
-  var day   = parts[2]; // months are 0-based
-  return new Date(year, month, day)
+// parse a date in yyyy-mm-dd-HH.MM format, e.g. 2013-11-18-21.00
+function parseDate(input) { 
+    var parts   = input.split('-');
+    var year    = parts[0];
+    var month   = parts[1]-1; // months are 0-based
+    var day     = parts[2]; 
+    var hours   = parts[3].split(".")[0];
+    var minutes = parts[3].split(".")[1];
+    
+    return new Date(year, month, day, hours, minutes);
+}
+
+function createUniqueDivId(dateString, movieId) {
+    var date = parseDate(dateString);
+    return strftime('%Y%m%dT%H%M', date)+"M"+movieId;
 }
